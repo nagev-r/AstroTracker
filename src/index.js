f3177fa5246b3a765f73d273534a278126b20ec3
nagev <nagevallon.r@gmail.com>
2024-02-06 20:36:58 -0500

    create blank base for webapp

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+function HelloWorld() {
+  return <h1 className="greeting">Hello, world for AstroTracker!</h1>;
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <HelloWorld />
   </React.StrictMode>
 );
 
